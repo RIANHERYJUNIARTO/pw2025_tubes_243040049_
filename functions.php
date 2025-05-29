23d7674596e14aec6d0a97a217345114c056b2ee
RIANHERYJUNIARTO <rianherijr@gmail.com>
2025-05-29 17:24:01 +0700

    proses

---
+++ functions.php
@@ -0,0 +1,53 @@
+<?php
+
+//koneksi database
+$conn = mysqli_connect('localhost','root','','pw2025_243040049_tubes');
+ function registrasi($data) {
+    global $conn;
+
+   $username = strtolower(trim(stripslashes($data["username"])));
+     $password = mysqli_real_escape_string($conn, $data["password"]);
+     $password2 = mysqli_real_escape_string($conn,$data["password2"] );
+      $email = mysqli_real_escape_string($conn, $data["email"]);
+
+      //cek username sudah ada atau belum
+     // $result = mysqli_query($conn,"SELECT username FROM  user WHERE username = '$username'");
+      $result = mysqli_query($conn, "SELECT username FROM users WHERE username = '$username'");
+      if( mysqli_fetch_assoc($result)) {
+        echo "<script> 
+        alert('username yang dipilih sudah terdaftar!')
+        </script>";
+        return false;
+      }
+
+     //cek konfirmasi password 
+     if($password !== $password2) {
+        echo "<script>
+        alert('konfirmasi password tidak sesuai!')
+        </script>";
+        return false;
+     }
+
+     //enksripsi password
+     $password =password_hash($password,PASSWORD_DEFAULT);
+     
+
+
+     //tambahkan user baru ke database
+    mysqli_query($conn, "INSERT INTO users (username, password, email) VALUES ('$username','$password','$email')");
+
+
+     return mysqli_affected_rows($conn);
+ }
+
+
+
+
+
+
+
+
+
+
+
+?>
