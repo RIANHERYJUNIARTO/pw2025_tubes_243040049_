70b8cdb651aec0d88327ab903a884632fa1a5587
RIANHERYJUNIARTO <rianherijr@gmail.com>
2025-06-07 11:49:25 +0700

    progres login dan register

---
+++ admin/function.php
@@ -0,0 +1,8 @@
+<?php
+// ----connect database---- //
+function()
+{
+    $conn = mysqli_connect('localhost','pw2025_tubes_243040049_',);
+    return $conn;
+}
+?>
--- registrasi.php
+++ registrasi.php
@@ -1,16 +1,16 @@
 <?php
 require 'functions.php';
 
-if(isset($_POST["register"]) ) {
+if (isset($_POST["register"])) {
 
-        if("registrasi"($_POST) > 0 ) {
-            echo "<script>
+    if ("registrasi"($_POST) > 0) {
+        echo "<script>
                     alert('user baru berhasil di tambahkan!');
                 </script>";
-        }else {
-            echo mysqli_error($conn);
-        }
+    } else {
+        echo mysqli_error($conn);
     }
+}
 
 
 ?>
@@ -18,48 +18,96 @@ if(isset($_POST["register"]) ) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registrasi</title>
-    <style> 
+    <style>
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 100vh;
+            background: #ddd;
+
+
+        }
+
+        .body-log {
+            color: #fff;
+            
+
+        }
+
+        .wrapper {
+            position: relative;
+            width: 400px;
+            height: 50px;
+            background: rgb(200, 67, 10);
+            border-radius: 10px;
+            padding: 20px;
+
+
+
+
+
+        }
+
+
+
         label {
             display: block;
             align-items: center;
         }
     </style>
 </head>
+
 <body>
-    <h1>Halaman Registrasi</h1>
+
 
     <form action="" method="post">
-    <ul>
-        <li>
-            <label for="username">Username</label>
-            <input type="text" name="username" id="username">
-        </li>
-        <li>
-            <label for="password">Pasword</label>
-            <input type="password" name="password" id="password">
-        </li>
-        <li>
-            <label for="password2"> konfirmasi Pasword</label>
-            <input type="password" name="password2" id="password2">
-        </li>
-        <li>
-            <label for="Email"> Email</label>
-            <input type="email" name="email" id="email">
-        </li>
-       
-        <li>
-            <button type="submit" name="register">Sokin
+        <ul>
+            <div class="body-log">
+                <h1>Halaman Registrasi</h1>
 
-            </button>
-        </li>
+                <li>
+                    <div class="wrapper">
 
 
+                        <label for="username">Username</label>
+                        <input type="text" name="username" id="username">
+                    </div>
+                </li>
+                <li>
+                    <div class="wrapper">
+                        <label for="password">Pasword</label>
+                        <input type="password" name="password" id="password">
+                    </div>
+                </li>
+                <li>
+                    <div class="wrapper">
+                        <label for="password2"> konfirmasi Pasword</label>
+                        <input type="password" name="password2" id="password2">
+                    </div>
+                </li>
+                <li>
+                    <div class="wrapper">
+                        <label for="Email"> Email</label>
+                        <input type="email" name="email" id="email">
+                    </div>
+                </li>
 
-    </ul>
+                <li>
+                    <button type="submit" name="register">Sokin
+
+                    </button>
+                </li>
+            </div>
+
+
+        </ul>
     </form>
 </body>
+
 </html>
